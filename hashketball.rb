f1acaa89843853b601de295b75fe456f36745f83
Brian Kao <bk1613@nyu.edu>
2019-09-08 21:12:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -163,7 +163,7 @@ def player_numbers(name = '')
       team_data.each do |attribute, data|
         if attribute = team_data[:players]
           data.each do |data_key|
-            
+            jersey_nums.push()
           end
         end
       end
