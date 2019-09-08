5010fc9ef259184aa3d6018befc272558e57b7be
Brian Kao <bk1613@nyu.edu>
2019-09-08 19:52:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -144,7 +144,7 @@ end
 def team_colors
   game_hash.each do |location, team_data|
     team_data.each do |team_name, colors|
-     # if team_data[:team_name] == name
+     if team_data[:team_name] == name
         binding.pry
         
         
