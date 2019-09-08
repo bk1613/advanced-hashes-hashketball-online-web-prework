03b5ed8aebdb879e96180fc1872d6352979dbc10
Brian Kao <bk1613@nyu.edu>
2019-09-08 23:06:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -288,6 +288,7 @@ def long_name_steals_a_ton?
         data.each do |player|
           if player[:player_name].length > current && player[:steals] > current_steal
             current = player[:player_name].length
+            
             long_name = player[:player_name]
           end
         end
