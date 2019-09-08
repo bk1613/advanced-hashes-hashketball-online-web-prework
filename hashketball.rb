e517a532a2e87c95b3e92638edbd00f046621743
Brian Kao <bk1613@nyu.edu>
2019-09-08 22:21:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -227,6 +227,7 @@ def most_points_scored
 
 def winning_team
   total_name_home = 0
+  total_name_away = 0
   team_name = ""
   game_hash.each do |location, team|
     team.each do |attribute, data|
