21146fa8b9ab2b43a4fbfff758da41ddda5385c5
Brian Kao <bk1613@nyu.edu>
2019-09-08 19:46:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -141,14 +141,14 @@ def shoe_size(name)
   
 end
 
-def team_colors(name = "")
+def team_colors
   game_hash.each do |location, team_data|
     team_data.each do |team_name, colors|
-      if team_data[:team_name] == name
+     # if team_data[:team_name] == name
         binding.pry
         
         
-      end
+      #end
     end
   end
 end
